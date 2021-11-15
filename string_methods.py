07bf66501aa706f89bdca6c5b06f1295856182a0
Qiu <aminzulfakar0@gmail.com>
2021-11-15 14:26:43 +0800

    Create string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,14 @@
+#string methods
+
+name = "chris patt"
+
+print(len(name))
+print(name.find("t"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("t"))
+print(name.replace("t", "s"))
+print(name*3)
