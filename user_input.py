5243f8d9e6abdff17bffc9bb1fcd49f6518d3dad
Qiu <aminzulfakar0@gmail.com>
2021-11-15 14:26:45 +0800

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,14 @@
+#user input
+
+# input("What is your name: ")
+
+name = input("What is your name: ")
+print("hello " + name)
+
+age = int(input("How old are you : "))
+age = age + 1
+print("Next year, you will be "+ str(age) + "years old")
+
+height = float(input("Give your height : "))
+print("Your height is " + str(height) + " cm")
+
