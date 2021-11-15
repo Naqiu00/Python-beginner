9707dc4dda93f5af065c109247b0f7e6b59ecd44
Qiu <aminzulfakar0@gmail.com>
2021-11-15 11:41:35 +0800

    Create multiple_assignment.py

---
+++ multiple_assignment.py
@@ -0,0 +1,9 @@
+#multiple assignment = allow us to assign multiple variable in one line or at the same time
+
+# name = "Chris"
+# age = 24
+# good_looking = True
+
+name, age, good_looking = "Chris", 24, True
+
+print(name, age, good_looking)
