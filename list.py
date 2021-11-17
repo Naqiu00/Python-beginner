b4e16a4aff2b64ea232d327e25ec9fe0da7c8741
Qiu <aminzulfakar0@gmail.com>
2021-11-17 08:44:57 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,18 @@
+#list = store multiple item in a single variable
+
+cars = ["fenari", "myvi", "axia"]
+
+print(cars[0], cars[1], cars[2])
+cars[0] = "iriz"
+
+print(cars[0] + "\n")
+
+# cars.append("viva")
+# cars.remove("myvi")
+# cars.pop()
+# cars.insert(0, "saga")
+# cars.sort()
+# cars.clear()
+
+for x in cars:
+    print(x)
