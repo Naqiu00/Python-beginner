160ef5ae0a379f723df180a1ce7f6667eec40fc6
Qiu <aminzulfakar0@gmail.com>
2021-11-17 22:34:09 +0800

    Create function.py

---
+++ function.py
@@ -0,0 +1,13 @@
+# function = a block of code executed only when call
+
+def hello(first_name, last_name, age):
+    print("Hello " + first_name + " " + last_name)
+    print("You are " + str(age) + " years old")
+    print("Have a nice day!")
+
+# my_name = "Bro"
+# hello("Chris")
+# hello("Dude")
+# hello(my_name)
+
+hello("Chris", "Patt", 21)
