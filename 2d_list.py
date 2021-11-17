6c8fc30c4f65c2452cfac95bd3f85d9addfc2225
Qiu <aminzulfakar0@gmail.com>
2021-11-17 08:45:02 +0800

    Create 2d_list.py

---
+++ 2d_list.py
@@ -0,0 +1,8 @@
+#2d list = list of list
+
+drinks = ["coffee", "sprite", "tea"]
+dinner = ["pizza", "spaghetti", "hotdog"]
+dessert = ["ice cream", "macaron"]
+
+meal = [drinks, dinner, dessert]
+print(meal[2][1])
