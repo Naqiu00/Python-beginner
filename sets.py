567bc6618226459354d0b65f64ed565036fcab83
Qiu <aminzulfakar0@gmail.com>
2021-11-17 08:45:10 +0800

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,16 @@
+#set = collection of unordered and unindexed data. No duplicate value
+
+things = {"pencil", "pencil", "pencil", "eraser", "spoon"}
+foods = {"burger", "hotdog", "pizza", "spoon"}
+
+# things.add("fork")
+# things.remove("spoon")
+# things.clear()
+# things.update(foods)
+# combine = things.union(foods)
+
+# print(things.difference(foods))
+# print(things.intersection(foods))
+
+# for x in combine:
+#     print(x)
