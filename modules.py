04f338f0873afd3092c5a4f4fd793e4b817468d9
Qiu <aminzulfakar0@gmail.com>
2021-11-23 18:07:52 +0800

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,13 @@
+# module = file containing python code. Function, classes, etc.
+# used with modular programming, which is to separate program into parts
+
+import modules_messages as msg
+# from modules_messages import hello, bye
+# from modules_messages import * # to import all function in modules
+
+msg.hello()
+msg.bye()
+
+# hello()
+
+# help("modules")
