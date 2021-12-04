c992dd12d7f21f2fc126aa4563ae9f9eb3406e7e
Qiu <aminzulfakar0@gmail.com>
2021-12-04 22:19:31 +0800

    Create time_module.py

---
+++ time_module.py
@@ -0,0 +1,24 @@
+# epoch = date and time when your computer measures system time
+
+import time
+
+# print(time.ctime(1000000))  # convert a time expressed in seconds since epoch to a readable string
+
+# print(time.time())          # return current seconds since epoch
+
+# print(time.ctime(time.time()))
+
+# time_object = time.localtime()
+# time_object = time.gmtime()
+# print(time_object)
+# local_time = time.strftime("%B %d %Y %H:%M:%S", time_object)
+# print(local_time)
+
+# time_string = "20 April, 2020"
+# time_object = time.strptime(time_string, "%d %B, %Y")
+# print(time_object)
+
+time_tuple = (2020, 4, 20, 4, 20, 0, 0, 0, 0)
+# time_string = time.asctime(time_tuple)
+time_string = time.mktime(time_tuple)
+print(time_string)
